37e66e0a874e4a0953ed968e6d6191a30aee4604
sakshi <sakshiadsul48@gmail.com>
2024-04-21 14:33:32 +0530

    intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'classic-editor';
+  // title = 'angular';
+  public Editor = ClassicEditor;
+}
